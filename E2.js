062661f4fd0ab007c3549814246cd43a314526e2
Aum <aum@dizayn.io>
2024-02-15 11:20:05 +0530

    exercise-2

---
+++ E2.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = BigInt(5555555555555555555555555555555555555555555);
+let c = "aum";
+let d = true;
+let e = undefined;
+let f = null;
+const g = {car:"audi" , model:"A101" , top_speed: 500 };
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
